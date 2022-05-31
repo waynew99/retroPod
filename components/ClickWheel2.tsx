605142cdd25cd40a593dbc6baaf48ae77c62542a
Wayne Wang <sw@middlebury.edu>
2022-05-31 15:58:28 -0400

    volume control done

--- components/ClickWheel2.tsx
+++ components/ClickWheel2.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, PanResponder, Animated, Dimensions, } from "react-native";
-import { useRef, useState } from "react";
+import { View, Text, StyleSheet, PanResponder, Animated, Dimensions, TouchableOpacity, } from "react-native";
+import { useRef, useState, useEffect } from "react";
 import Icon from "../icon";
 import Svg, {
   G,
@@ -7,6 +7,11 @@ import Svg, {
   LinearGradient,
   Stop
 } from "react-native-svg";
+import * as Haptics from 'expo-haptics';
+import NativeIconicIcon from "./NativeIconicIcon";
+import { Audio } from 'expo-av';
+import { Sound } from "expo-av/build/Audio";
+
 
 const screenWidth = Math.round(Dimensions.get("window").width);
 const screenHeight = Math.round(Dimensions.get("window").height);
@@ -19,42 +24,100 @@ const convertXYToAngle = (x: number, y: number, center: { x: number, y: number }
 };
 
 export default function ClickWheel2() {
-  const position = useRef(new Animated.ValueXY()).current;
   const [currAngle, setCurrAngle] = useState(0);
   const [initialAngle, setInitialAngle] = useState(0);
-  
-  const angleOffset = currAngle - initialAngle;
-  
+  const [currRangeIndex, setCurrRangeIndex] = useState(0);
+  const [lastRangeIndex, setLastRangeIndex] = useState(0);
 
-  console.log("nominal diff: ", currAngle - initialAngle);
-  console.log("actual diff: ", angleOffset);
+  const [mySound, setMySound] = useState<Sound | null>(null);
+  const [playing, setPlaying] = useState(false);
 
+
+  const angleDiff = currAngle - initialAngle;
+
+  if (Math.floor(angleDiff / 10) !== currRangeIndex) {
+    setCurrRangeIndex(Math.floor(angleDiff / 10));
+    Haptics.selectionAsync();
+  }
+
+  const playSound = async () => {
+    if (mySound) {
+      await mySound.playAsync();
+    } else {
+      console.log('Loading Sound');
+      const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/杨宗纬-最爱.mp3'));
+      setMySound(sound);
+      await sound.playAsync();
+    }
+    console.log('Playing Sound');
+    setPlaying(true);
+
+  }
+
+  const pause = async () => {
+    if (mySound) {
+      await mySound.pauseAsync();
+      setPlaying(false);
+    }
+  }
+
+  const tryChangeVolume = async (increment: boolean) => {
+    if (mySound) {
+      const status = await mySound.getStatusAsync();
+      if (status.isLoaded) {
+        const newVolume = increment ? status.volume + 1 / 36 : status.volume - 1 / 36;
+        if (newVolume >= 0 && newVolume <= 1) {
+          console.log('Changing Volume to: ', newVolume);
+          await mySound.setVolumeAsync(newVolume);
+        } else {
+          console.log('Volume out of range');
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    return mySound ? () => {
+      console.log('Unloading Sound');
+      mySound.unloadAsync();
+    } : undefined;
+  }, [mySound]);
+
+  useEffect(() => {
+    if (mySound) {
+      console.log(currRangeIndex - lastRangeIndex);
+      if (currRangeIndex !== lastRangeIndex && currRangeIndex !== 0) {
+        if (Math.abs(currRangeIndex - lastRangeIndex) === 1) {
+          console.log('try changing Volume: ', currRangeIndex, lastRangeIndex);
+          tryChangeVolume(currRangeIndex > lastRangeIndex);
+        }
+        setLastRangeIndex(currRangeIndex);
+      }
+    }
+  }, [currRangeIndex]);
 
   return (
     <View style={styles.container}>
       <Animated.View
         style={[styles.wheel, {
-          //transform: position.getTranslateTransform()
-          transform: [{ rotate: currAngle.toString() + "deg" }]
+          transform: [{ rotate: angleDiff.toString() + "deg" }]
         }]}
         {...PanResponder.create({
           onStartShouldSetPanResponder: () => true,
           onPanResponderStart: (e, gesture) => {
             setInitialAngle(convertXYToAngle(gesture.x0, gesture.y0, { x: screenWidth / 2, y: screenHeight / 2 }));
+            setCurrAngle(convertXYToAngle(gesture.x0, gesture.y0, { x: screenWidth / 2, y: screenHeight / 2 }));
           },
           onPanResponderMove: (e, gesture) => {
-            position.setValue({ x: gesture.moveX, y: gesture.moveY });
-            console.log(gesture.moveX, gesture.moveY);
-
-            
             setCurrAngle(convertXYToAngle(gesture.moveX, gesture.moveY, { x: screenWidth / 2, y: screenHeight / 2 }));
           },
           onPanResponderRelease: (e, gesture) => {
-            //setLastPosition({ x: lastPosition.x + gesture.dx, y: lastPosition.y + gesture.dy });
-            position.setValue({ x: 0, y: 0 });
+            setCurrAngle(0);
+            setInitialAngle(0);
           }
         }).panHandlers}
       >
+       {/*} 
         <Svg width="100%" height="100%" viewBox="0 0 955 955">
           <G id="layer_2">
             <G id="layer_1-2">
@@ -63,17 +126,23 @@ export default function ClickWheel2() {
                 <Stop offset="0.2815" stopColor="#3D8CCB" />
                 <Stop offset="0.5674" stopColor="#1E80C6" />
                 <Stop offset="0.8652" stopColor="#0060ff" />
-                {/*<Stop offset="0.8652" stopColor="#0060AD"/>*/}
+                <Stop offset="0.8652" stopColor="#0060AD"/>
                 <Stop offset="0.9944" stopColor="#004886" />
               </LinearGradient>
               <Path fill="url(#SVGID_1_)" d="M477.4,955C214.1,955,0,740.9,0,477.6S214.1,0.3,477.4,0.3s477.4,214.1,477.4,477.4S740.6,955,477.4,955z
 	 M477.4,178.6c-164.9,0-299,134.1-299,299s134.1,299,299,299s299-134.1,299-299S642.2,178.6,477.4,178.6z"/>
             </G>
           </G>
-        </Svg>
+      </Svg>*/}
 
       </Animated.View>
       <View style={styles.wheelCenter}>
+        <TouchableOpacity onPress={() => {
+          if (playing) pause();
+          else playSound();
+        }}>
+          <NativeIconicIcon name="play-outline" size={wheelCenterRadius} color="#aaa" />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -84,6 +153,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    position: "absolute",
+    top: -300,
+    fontSize: 30,
+    color: '#000',
   },
   wheel: {
     position: 'absolute',
@@ -98,6 +173,9 @@ const styles = StyleSheet.create({
     height: wheelCenterRadius * 2,
     borderRadius: wheelCenterRadius,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   touch: {
     width: 30,
